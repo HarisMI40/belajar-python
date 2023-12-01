492caa20a07929eaba36bd4b12855f6aa89591ba
HarisMI40 <86542926+HarisMI40@users.noreply.github.com>
2023-12-01 16:35:48 +0700

    tambah file login dan style css

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+<div class="w-25 p-3">
+  <label for="exampleFormControlInput1" class="form-label">Email </label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="login">
+</div>
+<div class="w-25 p-3">
+  <label for="exampleFormControlInput1" class="form-label">Password </label>
+  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
+  <button type="button" class="btn btn-outline-primary">Login</button>
+</div>
+</body>
+</html>
